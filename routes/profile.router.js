fd7b4d89a230a7f034e040e2f566632a92e17b74
Marx Chryz <marxchryz30@gmail.com>
2021-06-22 19:01:00 +0800

    sameUser middleware added

--- routes/profile.router.js
+++ routes/profile.router.js
@@ -2,12 +2,14 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 const upload = multer({ dest: './public/apps/uploads/' });
+const { jwtAuth, sameUser } = require('../middlewares');
 const { profileController } = require('../controllers');
 
+router.use(jwtAuth);
 router
   .route('/:userId')
   .get(profileController.getOne)
-  .post([upload.single('file')], profileController.post);
+  .post([sameUser, upload.single('file')], profileController.post);
 router.route('/').get(profileController.get);
 
 module.exports = router;
