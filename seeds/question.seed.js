31c21d612b3162adcda23bd1d3aafd1a7268132c
Marx Chryz <marxchryz30@gmail.com>
2021-06-28 23:16:27 +0800

    questions seeds updated

--- seeds/question.seed.js
+++ seeds/question.seed.js
@@ -1,9 +1,10 @@
 const { Question } = require('../models');
 
 module.exports = async (req, res) => {
+  await Question.deleteMany({});
   let questions = [
     {
-      question: 'How long do you anticipate your next relationship lasting?',
+      question: '1. How long do you anticipate your next relationship lasting?',
       choices: [
         'a. For one night',
         'b. For several months to a year',
@@ -12,20 +13,16 @@ module.exports = async (req, res) => {
       ],
     },
     {
-      question: 'Which term best describes you?',
-      choices: ['a. Playful', 'b. Dramatic'],
-    },
-    {
-      question: 'What role does religion/God play in your life?',
+      question: '2. What role does religion/God play in your life?',
       choices: [
-        'a. Critical',
-        'b. Significant',
-        'c. Not significant',
-        'd. Not significant at all',
+        'a. Extremely important',
+        'b. Somewhat important',
+        'c. Not very important',
+        'd. Not important at all',
       ],
     },
     {
-      question: 'Are you prepared to settle down and marry immediately?',
+      question: '3. Are you prepared to settle down and marry immediately?',
       choices: [
         'a. Without a doubt',
         'b. In no way',
@@ -34,110 +31,28 @@ module.exports = async (req, res) => {
       ],
     },
     {
-      question: 'Are you a fan of political debates?',
-      choices: ['a. Yes', 'b.No'],
-    },
-    {
-      question: 'Would you date someone who owed a lot of money?',
-      choices: [
-        'a. Absolutely, it is not an issue for me',
-        'b. Yes, if I believed the debt was justified',
-        'c. Hell no',
-      ],
-    },
-    {
-      question: 'Is the astrological sign significant in a match?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Which of the following best describes your political views?',
-      choices: ['a. Liberal', 'b. Centrist', 'c. Conservative', 'd. Others'],
-    },
-    {
-      question: 'Could you date someone who was extremely unorganized?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Which one would you prefer to be?',
-      choices: ['a. Ordinary', 'b. Crazy'],
-    },
-    {
-      question: 'Are you employed at the moment?',
-      choices: [
-        'a. Yes, I work full-time',
-        'b. Yes, I work part-time',
-        'c. No',
-        'd. I am currently studying',
-      ],
-    },
-    {
-      question: 'Select the more romantic activity.',
-      choices: ['a. Kissing in Paris', 'b. Kissing in the woods, in a tent'],
-    },
-    {
-      question: 'Is jealousy a healthy emotion to have in a relationship?',
-      choices: ['a. Yes', 'b. Depends upon the situation', 'c. No'],
-    },
-    {
-      question: 'Are you a fan of scary films?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
       question:
-        'Do you frequently find yourself worrying about events over which you have no control?',
+        '4. Do you believe it is necessary to establish an emotional connection before establishing a physical one?',
       choices: ['a. Yes', 'b. No'],
     },
     {
-      question:
-        'Do you believe it is necessary to establish an emotional connection before establishing a physical one?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Is it acceptable to support racial equality silently?',
-      choices: [
-        'a. Yes, that is your choice',
-        'b. No, it is critical that your voice be heard.',
-      ],
-    },
-    {
-      question: 'Are you going to receive the COVID-19 vaccine?',
-      choices: ['a. Yes', 'b. No', 'c. I am unsure'],
-    },
-    {
-      question: 'Would you tell someone you\'ve never met "I love you"?',
+      question: '5. Would you tell someone you\'ve never met "I love you"?',
       choices: [
         'a. Certainly, if it felt right.',
         'b. No, I require a physical connection first.',
       ],
     },
     {
-      question: 'What is the best icebreaker?',
-      choices: [
-        'a. Posing a question',
-        'b. Sharing a joke',
-        'c. Making a brief introduction',
-        'd. Mentioning something on my profile',
-      ],
-    },
-    {
-      question: 'What would you do if the world ended in a zombie apocalypse?',
-      choices: ['a. Run', 'b. Retaliate'],
-    },
-    {
-      question: 'Do you have a voter registration card?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
       question:
-        'Whatever your future plans, what interests you more right now?',
+        '6. Whatever your future plans, what interests you more right now?',
       choices: ['a. Sex', 'b. Love'],
     },
     {
-      question: 'Is a soulmate worth waiting?',
+      question: '7. Is a soulmate worth waiting?',
       choices: ['a. Yes', 'b. No'],
     },
     {
-      question: 'Is it critical for you to feel at ease in a relationship?',
+      question: '8. Is it critical for you to feel at ease in a relationship?',
       choices: [
         'a. Without a doubt, total comfort is critical.',
         "b. Yes, but I'm looking for someone who will push me.",
@@ -145,105 +60,7 @@ module.exports = async (req, res) => {
       ],
     },
     {
-      question: 'Do you enjoy brunching?',
-      choices: ['a. Yes', 'b. Not really'],
-    },
-    {
-      question: 'When a plane lands, do you clap?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question:
-        'Are you of the opinion that entertainers should be held accountable for jokes made decades ago?',
-      choices: [
-        'a. Yes, they must be made responsible.',
-        "b. No, as long as the jokes were amusing, it's acceptable.",
-      ],
-    },
-    {
-      question: 'Could you date someone who is anti-vaccination?',
-      choices: ['a. Hmmmm, yes', 'b. Nooo!!!'],
-    },
-    {
-      question: 'Does climate change concren you?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Are you an activist?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question:
-        "Are family planning benefits a priority for you when you're job hunting?",
-      choices: ['a. Definitely', 'b. Not at all'],
-    },
-    {
-      question: 'Would you date someone who vaped/used electronic cigarettes?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question:
-        'How do you feel when you see people who are engaged in your social media accounts?',
-      choices: ['a. Kudos to them', 'b. Admittedly a little envious'],
-    },
-    {
-      question:
-        'How comfortable are you with discussing politics with your family?',
-      choices: ['a. It is critical', 'b. Certainly not'],
-    },
-    {
-      question: 'Shower sex?',
-      choices: ['a. Yes, please', 'b. Thank you, but no'],
-    },
-    {
-      question: 'Is pizza one of your top five favorite foods?',
-      choices: ['a. Yes', 'b. Nope'],
-    },
-    {
-      question:
-        'After the pandemic, which ""first"" makes you the most nervous?',
-      choices: [
-        'a. The very first date',
-        'b. The very first kiss',
-        'c. The very first sleepover',
-        'd. The very first meeting with his/her friends/family',
-      ],
-    },
-    {
-      question: 'Is punctuality important to you?',
-      choices: ['a. Without a doubt', 'b. Not at all'],
-    },
-    {
-      question: 'Do you believe sabong should be prohibited?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Do you think pool is a good first date activity?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Do you enjoy shopping at the mall?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Dinuguan?',
-      choices: ['a. Definitely', 'b. No, thanks'],
-    },
-    {
-      question: 'Do you think eating kamayan on a date is appropriate?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question:
-        'Would you kiss your partner on the lips in public if you were in a relationship?',
-      choices: ['a. Yes', 'b. No'],
-    },
-    {
-      question: 'Is it possible to date someone who does not vote?',
-      choices: ['a. Yes', 'b. Certainly not'],
-    },
-    {
-      question: 'What is the best first date?',
+      question: '9. What is the best first date?',
       choices: [
         'a. Coffee',
         'b. Something with a cultural or activity component',
@@ -252,17 +69,35 @@ module.exports = async (req, res) => {
       ],
     },
     {
+      question: '10.  Shower sex?',
+      choices: ['a. Yes, please', 'b. No'],
+    },
+    {
       question:
-        'Is there anyone you know who has married someone they met through a dating app?',
-      choices: ['a. Yes', 'b. No'],
+        '11.  Which of the following best describes your political views?',
+      choices: ['a. Liberal', 'b. Centrist', 'c. Conservative', 'd. Others'],
     },
     {
-      question: 'Do you want your date to share your political beliefs?',
-      choices: ['a. Yes', 'b. No'],
+      question:
+        '12.  How would you feel if your partner earned more money than you?',
+      choices: ['a. Jealous', 'b. Indifferent', 'c. Inferior', 'd. Thrilled'],
     },
     {
-      question: 'The right to freedom of the press is…',
-      choices: ['a. Important', 'b. Unimportant'],
+      question: '13.  Would you be open to a long-distance relationship?',
+      choices: [
+        'a. Yes, for the right person we can make it work',
+        'b. It depends on how far away they are',
+        "c. No, I can't do long distance",
+      ],
+    },
+    {
+      question: '14.  Do you support Duterte war on drugs?',
+      choices: ['a. Yes, it is what we need', 'b. No, it is too harsh'],
+    },
+    {
+      question:
+        'Would you have children with a partner if you were not married?',
+      choices: ['a. Yes', 'b. No'],
     },
   ];
   questions = questions.map((question) => {
--- seeds/test.seed.js
+++ seeds/test.seed.js
@@ -6,39 +6,285 @@ const {
   Message,
   Question,
 } = require('../models');
+const faker = require('faker');
+
+const random = (arr, min = 1, max = 1) => {
+  let chosen = [];
+  let n = Math.floor(Math.random() * (max + 1 - min)) + min;
+  for (let i = 0; i < n; i++) {
+    let index = Math.floor(Math.random() * arr.length);
+    chosen.push(arr[index]);
+    arr.splice(index, 1);
+  }
+  if (min === max && min === 1) {
+    return chosen[0];
+  }
+  return chosen;
+};
 
 module.exports = async () => {
   await Interest.deleteMany({});
-  await Question.deleteMany({});
   await Evaluation.deleteMany({});
   await Match.deleteMany({});
   await Message.deleteMany({});
 
-  let questions = [];
-  'abcde'.split('').map((question) => {
-    questions.push({
-      isImportant: false,
-      question,
-      choices: [],
-    });
-  });
-  questions = await Question.create(questions);
+  let questions = await Question.find().limit(10);
+  let temp = questions;
   questions = questions.map((question) => question._id);
+
   let users = await User.find();
   for (let i = 0; i < users.length; i++) {
     let user = users[i];
+    let answers = temp.map((question) => random(question.choices));
+    let astrologicalSign = random([
+      'aries',
+      'taurus',
+      'gemini',
+      'cancer',
+      'leo',
+      'virgo',
+      'libra',
+      'scorpio',
+      'sagittarius',
+      'capricorn',
+      'aquarius',
+      'pisces',
+    ]);
+    let religion = random([
+      'no religion',
+      'roman catholic',
+      'islam',
+      'other christian',
+      'tribal religion',
+      'others',
+      'protestant',
+    ]);
+    let politicalView = random([
+      'liberalism',
+      'conservatism',
+      'socialism',
+      'social democracy',
+      'communism',
+      'fascism',
+      'feminism',
+    ]);
+    let drinks = random([
+      'milk',
+      'coffee',
+      'chocolate',
+      'tea',
+      'mixed fruit',
+      'juice',
+      'energy drink',
+      'soft drinks',
+      'beer',
+      'wine',
+      'others',
+    ]);
+    let smoke = random([
+      "I don't smoke",
+      'Sometimes',
+      'Cigarettes',
+      'Light and Menthol Cigarettes',
+      'E-cigarettes',
+    ]);
+    let wantKids = random([
+      'might be interested in having children',
+      'interested in having children',
+      'not interested in having children',
+      'want to adopt a child',
+    ]);
+    let wantMarried = random([
+      "I don't want to get married",
+      'Religious Wedding',
+      'Civil Wedding',
+      'Formal Wedding',
+      'Informal Wedding',
+      'Destination Wedding',
+      'Cruise Wedding',
+      'Vintage Style Wedding',
+    ]);
+    let pets = random(
+      [
+        'Amphibians',
+        'Birds',
+        'Cats',
+        'Dogs',
+        'Ferrets',
+        'Fish',
+        'Guinea pigs',
+        'Horses',
+        'Rabbits',
+        'Rats and mice',
+        'Reptiles',
+      ],
+      1,
+      3
+    );
+    let sports = random(
+      [
+        'Baseball',
+        'Basketball',
+        'Cricket',
+        'Football',
+        'Golf',
+        'Gymnastics',
+        'Hockey',
+        'Martial Arts',
+        'Swimming',
+        'Table Tennis',
+        'Tennis',
+        'Volleyball',
+      ],
+      1,
+      3
+    );
+    let hobbies = random(
+      [
+        'Arts and Crafts',
+        'Cooking',
+        'Exercising',
+        'Fishing',
+        'Gaming',
+        'Gardening',
+        'Gardening',
+        'Hiking',
+        'Music',
+        'Reading',
+        'Traveling',
+        'Watching',
+        'Yoga',
+      ],
+      1,
+      3
+    );
+    let musicGenre = random(
+      [
+        'Classical',
+        'Woman playing violin classical music',
+        'Country',
+        'Electronic dance music (EDM)',
+        'Hip-hop',
+        'Indie rock',
+        'Jazz',
+        'J-pop',
+        'K-pop',
+        'Metal',
+        'Oldies',
+        'OPM',
+        'Pop',
+        'Rap',
+        'Rhythm & blues (R&B)',
+        'Rock',
+        'Techno',
+      ],
+      1,
+      3
+    );
+    let movieGenre = random(
+      [
+        'Action',
+        'Adventure',
+        'Animation',
+        'Comedy',
+        'Crime and mystery',
+        'Fantasy',
+        'Historical',
+        'Horror',
+        'Romance',
+        'Satire',
+        'Science fiction',
+        'Cyberpunk and derivatives',
+        'Speculative',
+        'Thriller',
+        'Western',
+      ],
+      1,
+      3
+    );
+    let books = random(
+      [
+        'Mystery',
+        'Science fiction',
+        'Historical Fiction',
+        'Literary Fiction',
+        'Young Adult Fiction',
+        'Horror Fiction',
+        'Children’s literature',
+        'Short story',
+        'Thriller',
+        'Memoir',
+        'Fantasy',
+        'Fairy tale',
+        'Suspense',
+        'Prose',
+        'Adventure Fiction',
+        'Drama',
+        'Detective Novel',
+        'Paranormal Romance',
+        'Romance Novel',
+        'Historical Romance',
+        'Graphic Novel',
+        'Philosophy',
+        'Crime Novel',
+        'True Crime',
+        'Mythology',
+        'History',
+        'Folklore',
+      ],
+      1,
+      3
+    );
+    let food = random(
+      [
+        'Street foods',
+        'Lumpiang Shanghai',
+        'Pansit',
+        'Pizza',
+        'Kare-Kare',
+        'Chicken Adobo',
+        'Pasta',
+        'Hamburger',
+        'Soup',
+        'Bread',
+        'Milk Tea',
+        'Halo-halo',
+        'Silog',
+        'Lechon',
+        'Sinigang',
+      ],
+      1,
+      3
+    );
+
     let interest = await Interest.create({
       userId: user._id,
       maxDistance: 90,
       minDistance: 10,
       minAge: 10,
       maxAge: 90,
+      genderPref: faker.lorem.word(),
+      astrologicalSign,
+      religion,
+      politicalView,
+      drinks,
+      smoke,
+      wantKids,
+      wantMarried,
+      pets,
+      sports,
+      hobbies,
+      musicGenre,
+      movieGenre,
+      books,
+      food,
     });
+
     await Evaluation.create({
       userId: user._id,
       interest: interest._id,
       questions,
-      answers: ['a', 'b', 'c', 'd', 'e'],
+      answers,
     });
 
     let currentId = user._id;
