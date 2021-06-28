712a6030efd9ffaaecdcdef330b3b865e1c73616
KurtA30 <53459193+KurtA30@users.noreply.github.com>
2021-06-28 14:59:17 +0800

    Add files via upload
    Changed gender to sex, Added postal code to address

--- models/user.model.js
+++ models/user.model.js
@@ -1,91 +1,95 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  mobileNumber: {
-    type: String,
-    default: '',
-  },
-  facebookId: {
-    type: String,
-    default: '',
-  },
-  gmail: {
-    type: String,
-    default: '',
-  },
-  orientation: {
-    type: String,
-    required: true,
-  },
-  sex: {
-    type: String,
-    default: '',
-  },
-  birthday: {
-    type: Date,
-    required: true,
-  },
-  showActiveStatus: {
-    type: Boolean,
-    default: true,
-  },
-  activeStatus: {
-    type: String,
-    enum: ['online', 'offline'],
-    default: 'online',
-  },
-  verificationStatus: {
-    type: String,
-    enum: ['verified', 'unverified', 'pending'],
-    default: 'unverified',
-  },
-  address: {
-    city: {
-      type: String,
-      default: '',
+    firstName: {
+        type: String,
+        required: true,
     },
-    province: {
-      type: String,
-      default: '',
+    lastName: {
+        type: String,
+        required: true,
     },
-    country: {
-      type: String,
-      default: '',
+    username: {
+        type: String,
+        required: true,
     },
-    latitude: {
-      type: String,
-      default: '',
+    mobileNumber: {
+        type: String,
+        default: '',
     },
-    longitude: {
-      type: String,
-      default: '',
+    facebookId: {
+        type: String,
+        default: '',
     },
-  },
-  employment: {
-    companyName: { type: String, default: '' },
-    position: { type: String, default: '' },
-  },
-  blockedUsers: {
-    type: [mongoose.Schema.Types.ObjectID],
-    default: [],
-    ref: 'User',
-  },
-  leftConversations: {
-    type: [mongoose.Schema.Types.ObjectID],
-    default: [],
-  },
+    gmail: {
+        type: String,
+        default: '',
+    },
+    sex: {
+        type: String,
+        default: '',
+    },
+    orientation: {
+        type: String,
+        required: true,
+    },
+    birthday: {
+        type: String,
+        required: true,
+    },
+    showActiveStatus: {
+        type: Boolean,
+        default: true,
+    },
+    activeStatus: {
+        type: String,
+        enum: ['online', 'offline'],
+        default: 'online',
+    },
+    verificationStatus: {
+        type: String,
+        enum: ['verified', 'unverified', 'pending'],
+        default: 'unverified',
+    },
+    address: {
+        postalCode: {
+            type: String,
+            default: '',
+        },
+        city: {
+            type: String,
+            default: '',
+        },
+        province: {
+            type: String,
+            default: '',
+        },
+        country: {
+            type: String,
+            default: '',
+        },
+        latitude: {
+            type: String,
+            default: '',
+        },
+        longitude: {
+            type: String,
+            default: '',
+        },
+    },
+    employment: {
+        type: String,
+        default: '',
+    },
+    blockedUsers: {
+        type: [mongoose.Schema.Types.ObjectID],
+        default: [],
+        ref: 'User',
+    },
+    leftConversations: {
+        type: [mongoose.Schema.Types.ObjectID],
+        default: [],
+    },
 });
 
 const User = mongoose.model('User', userSchema);
