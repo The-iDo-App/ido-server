cae872eee48136647bcbcc8d8b42e9819f92b863
KurtA30 <arellanokurt0130@gmail.com>
2021-12-31 17:12:19 +0800

    changes in registration

--- startup/app.js
+++ startup/app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 module.exports = app;
